8573c152b8afa119fea3bc461fc3439a050506e8
Vikram Kangotra <vikramkangotra8055@gmail.com>
2022-07-16 01:29:41 +0530

    created token file

---
+++ src/token.rs
@@ -0,0 +1,33 @@
+#[derive(Debug,PartialEq)]
+pub enum TokenType {
+    // Single-character tokens.
+    LeftParen, RightParen, LeftBrace, RightBrace,
+    Comma, Dot, Minus, Plus, Semicolon, Slash, Star,
+    // One or two character tokens.
+    Bang, BangEqual,
+    Equal, EqualEqual,
+    Greater, GreaterEqual,
+    Less, LessEqual,
+    // Literals.
+    Identifier, StringLiteral, NumberLiteral,
+    // Keywords.
+    And, Class, Else, False, Fun, For, If, Nil, Or,
+    Print, Return, Super, This, True, Var, While,
+    // End of file.
+    Eof, Unknown
+}
+
+#[derive(Debug)]
+pub struct Token {
+    pub token_type: TokenType,
+    pub literal: String,
+}
+
+impl Token {
+    pub fn new(token_type: TokenType, literal: String) -> Token {
+        Token {
+            token_type,
+            literal,
+        }
+    }
+}

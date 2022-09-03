f195b880a7b8488ba3c0d12bef1e4501de225a07
Vikram Kangotra <vikramkangotra8055@gmail.com>
2022-09-03 18:27:15 +0530

    now includes stmt classes

--- src/expr.rs
+++ src/expr.rs
@@ -1,38 +1,38 @@
 use crate::token::Token;
 
 pub trait ExprVisitor {
-    fn visit_literal(&mut self, literal: &Literal) -> f64;
-    fn visit_unary(&mut self, unary: &Unary) -> f64;
-    fn visit_binary(&mut self, binary: &Binary) -> f64;
+    fn visit_literal(&mut self, literal: &LiteralExpr) -> f64;
+    fn visit_unary(&mut self, unary: &UnaryExpr) -> f64;
+    fn visit_binary(&mut self, binary: &BinaryExpr) -> f64;
 }
 
 pub trait Expr {
     fn accept(&self, visitor: &mut dyn ExprVisitor) -> f64;
 }
 
-pub struct Literal {
+pub struct LiteralExpr {
     token: Token,
 }
 
-impl Literal {
+impl LiteralExpr {
     pub fn new(token: Token) -> Self {
         Self {  token, }
     }
     pub fn get_token(&self) -> &Token { &self.token }
 }
 
-impl Expr for Literal {
+impl Expr for LiteralExpr {
     fn accept(&self, visitor: &mut dyn ExprVisitor) -> f64 {
         return visitor.visit_literal(self);
     }
 }
 
-pub struct Unary {
+pub struct UnaryExpr {
     op: Token,
     right: Box<dyn Expr>,
 }
 
-impl Unary {
+impl UnaryExpr {
     pub fn new(op: Token, right: Box<dyn Expr>) -> Self {
         Self {  op,  right, }
     }
@@ -40,19 +40,19 @@ impl Unary {
     pub fn get_right(&self) -> &Box<dyn Expr> { &self.right }
 }
 
-impl Expr for Unary {
+impl Expr for UnaryExpr {
     fn accept(&self, visitor: &mut dyn ExprVisitor) -> f64 {
         return visitor.visit_unary(self);
     }
 }
 
-pub struct Binary {
+pub struct BinaryExpr {
     left: Box<dyn Expr>,
     op: Token,
     right: Box<dyn Expr>,
 }
 
-impl Binary {
+impl BinaryExpr {
     pub fn new(left: Box<dyn Expr>, op: Token, right: Box<dyn Expr>) -> Self {
         Self {  left,  op,  right, }
     }
@@ -61,9 +61,52 @@ impl Binary {
     pub fn get_right(&self) -> &Box<dyn Expr> { &self.right }
 }
 
-impl Expr for Binary {
+impl Expr for BinaryExpr {
     fn accept(&self, visitor: &mut dyn ExprVisitor) -> f64 {
         return visitor.visit_binary(self);
+    }
+}
+
+pub trait StmtVisitor {
+    fn visit_expression(&mut self, expression: &ExpressionStmt) -> f64;
+    fn visit_print(&mut self, print: &PrintStmt) -> f64;
+}
+
+pub trait Stmt {
+    fn accept(&self, visitor: &mut dyn StmtVisitor) -> f64;
+}
+
+pub struct ExpressionStmt {
+    expression: Box<dyn Expr>,
+}
+
+impl ExpressionStmt {
+    pub fn new(expression: Box<dyn Expr>) -> Self {
+        Self {  expression, }
+    }
+    pub fn get_expression(&self) -> &Box<dyn Expr> { &self.expression }
+}
+
+impl Stmt for ExpressionStmt {
+    fn accept(&self, visitor: &mut dyn StmtVisitor) -> f64 {
+        return visitor.visit_expression(self);
+    }
+}
+
+pub struct PrintStmt {
+    expression: Box<dyn Expr>,
+}
+
+impl PrintStmt {
+    pub fn new(expression: Box<dyn Expr>) -> Self {
+        Self {  expression, }
+    }
+    pub fn get_expression(&self) -> &Box<dyn Expr> { &self.expression }
+}
+
+impl Stmt for PrintStmt {
+    fn accept(&self, visitor: &mut dyn StmtVisitor) -> f64 {
+        return visitor.visit_print(self);
     }
 }
 
